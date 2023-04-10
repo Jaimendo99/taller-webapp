25ab627fed32018c73b17b39683138afe53daec7
jaime <105898917+Jaimendo99@users.noreply.github.com>
2023-04-10 15:57:49 -0500

    Initialized repo

---
+++ taller-mvc/taller-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace taller_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
